51cdd64aa548b7d3cba910897e2e9e63f1e65efc
wuhan005 <524306184@qq.com>
2017-05-17 13:53:34 +0800

    Upload Project.
    Signed-off-by: wuhan005 <524306184@qq.com>

---
+++ tupian114 downloader/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace tupian114_downloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backResult = new System.Windows.Forms.LinkLabel();
+            this.linkWebsite = new System.Windows.Forms.LinkLabel();
+            this.linkGithub = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.submit.Location = new System.Drawing.Point(637, 42);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Hunt it!";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "图片114素材链接";
+            // 
+            // textURL
+            // 
+            this.textURL.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textURL.Location = new System.Drawing.Point(113, 13);
+            this.textURL.Name = "textURL";
+            this.textURL.Size = new System.Drawing.Size(599, 23);
+            this.textURL.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Developer: John Wu";
+            // 
+            // backResult
+            // 
+            this.backResult.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.backResult.Location = new System.Drawing.Point(14, 79);
+            this.backResult.Name = "backResult";
+            this.backResult.Size = new System.Drawing.Size(690, 46);
+            this.backResult.TabIndex = 6;
+            this.backResult.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.backResult_LinkClicked);
+            // 
+            // linkWebsite
+            // 
+            this.linkWebsite.AutoSize = true;
+            this.linkWebsite.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkWebsite.Location = new System.Drawing.Point(13, 175);
+            this.linkWebsite.Name = "linkWebsite";
+            this.linkWebsite.Size = new System.Drawing.Size(121, 17);
+            this.linkWebsite.TabIndex = 8;
+            this.linkWebsite.TabStop = true;
+            this.linkWebsite.Text = "Website: wuhan5.cc";
+            this.linkWebsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkWebsite_LinkClicked);
+            // 
+            // linkGithub
+            // 
+            this.linkGithub.AutoSize = true;
+            this.linkGithub.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkGithub.Location = new System.Drawing.Point(12, 194);
+            this.linkGithub.Name = "linkGithub";
+            this.linkGithub.Size = new System.Drawing.Size(46, 17);
+            this.linkGithub.TabIndex = 9;
+            this.linkGithub.TabStop = true;
+            this.linkGithub.Text = "Github";
+            this.linkGithub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkGithub_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(425, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(299, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "本软件仅供学习交流使用,本软件作者不承担一切责任。";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 212);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkGithub);
+            this.Controls.Add(this.linkWebsite);
+            this.Controls.Add(this.backResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "图片114 免费下载器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel backResult;
+        private System.Windows.Forms.LinkLabel linkWebsite;
+        private System.Windows.Forms.LinkLabel linkGithub;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ tupian114 downloader/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tupian114_downloader
+{
+    public partial class MainForm : Form
+    {
+        private string orginalURL;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            if (textURL.Text == "")
+            {
+                MessageBox.Show("请输入链接！");
+            }
+            else
+            {
+                orginalURL = textURL.Text;
+                orginalURL = orginalURL.Substring(orginalURL.IndexOf("_") + 1, (orginalURL.IndexOf(".html") - orginalURL.IndexOf("_")));
+
+                backResult.Text = getDownload("http://www.tupian114.com/download/down?tid=" + orginalURL + "&type=2");
+            }
+            }
+
+        private String getDownload(string url)
+        {
+            WebClient webConn = new WebClient();
+            webConn.Headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            webConn.Headers.Add("Host", "www.tupian114.com");
+            webConn.Headers.Add("Referer", "http://www.tupian114.com/download/" + orginalURL + ".html");
+
+            Stream data = webConn.OpenRead(url);
+            StreamReader reader = new StreamReader(data, Encoding.UTF8);
+            string backValue = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            backValue = backValue.Replace("\"", "");
+            return backValue;
+
+        }
+
+        private void linkWebsite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://wuhan5.cc/");
+        }
+
+        private void linkGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/wuhan005/tupian114-downloader");
+        }
+
+        private void backResult_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(backResult.Text);
+        }
+    }
+}
